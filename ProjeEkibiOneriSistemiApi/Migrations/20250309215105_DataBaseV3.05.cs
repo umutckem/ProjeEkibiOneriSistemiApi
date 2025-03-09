25b56d805698906e5cf6f2a3af2eece44b8198d9
Umutcan Kemahlı <130008707+RagnarLorthbro@users.noreply.github.com>
2025-03-10 00:59:25 +0300

    V5

---
+++ ProjeEkibiOneriSistemiApi/Migrations/20250309215105_DataBaseV3.05.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjeEkibiOneriSistemiApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataBaseV305 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KategoriId",
+                table: "kullaniciYanitlari",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KategoriId",
+                table: "kullaniciYanitlari");
+        }
+    }
+}
--- ProjeEkibiOneriSistemiApi/Models/KullaniciYaniti.cs
+++ ProjeEkibiOneriSistemiApi/Models/KullaniciYaniti.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public Guid OgrenciId { get; set; }
+        public int KategoriId { get; set; }
         public int SoruId { get; set; }
         public int Puan { get; set; } // 1-10 arasında verilen puan
 
