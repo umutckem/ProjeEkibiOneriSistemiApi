9beed6a932ace75fdd608b56c26808191316194e
Umutcan Kemahli <130008707+RagnarLorthbro@users.noreply.github.com>
2025-06-09 03:14:52 +0300

    V6,01

---
+++ ProjeEkibiOneriSistemiApi/Migrations/20250608224319_DataBaseV3.05.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjeEkibiOneriSistemiApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataBaseV305 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IlgiliBolum",
+                table: "kategoriler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IlgiliBolum",
+                table: "kategoriler");
+        }
+    }
+}
--- ProjeEkibiOneriSistemiApi/Models/Kategori.cs
+++ ProjeEkibiOneriSistemiApi/Models/Kategori.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Ad { get; set; } // Kategorinin adı, örn: "C#", "Veritabanı", "Algoritma"
+        public string IlgiliBolum { get; set; }
 
     }
 }
